c0ec7737ef64c6330714260029acf6376470fcc3
Ryan Ashton <djryanash@me.com>
2022-12-28 18:04:46 +0800

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Ryan Ashton on 28/12/2022.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print ("Doing some work...")
+    }
+}
